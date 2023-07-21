1397b52ec946c7f96ab87891be53d174fc0b4eee
unknown <rafael.g.cavalheiro@gmail.com>
2023-07-21 04:22:03 -0300

    vue integrado com sucesso

--- resources/js/TestComponent.vue
+++ resources/js/TestComponent.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <h2>Teste de Integração Vue.js com Laravel</h2>
-    <p>Se a exibição desta mensagem estiver funcionando, a integração foi bem-sucedida!</p>
+    <h2>Test Component</h2>
+    <p>Este é um exemplo de componente Vue.js com Laravel Mix.</p>
+    <button @click="showMessage">Clique para mostrar o alerta</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TestComponent'
-}
+  data() {
+    return {
+      message: "Olá, este é um componente Vue simples!",
+    };
+  },
+  methods: {
+    showMessage() {
+      alert(this.message);
+    },
+  },
+};
 </script>
+
+<style>
+h2 {
+  color: blue;
+}
+
+p {
+  font-size: 16px;
+}
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,4 @@
 const mix = require('laravel-mix');
-const { VueLoaderPlugin } = require('vue-loader');
 
-mix.js('resources/js/app.js', 'public/js')
-   .webpackConfig({
-      module: {
-         rules: [
-            {
-               test: /\.vue$/,
-               loader: 'vue-loader'
-            },
-            {
-               test: /\.js$/,
-               loader: 'babel-loader',
-               exclude: /node_modules/
-            }
-         ]
-      },
-      plugins: [
-         new VueLoaderPlugin()
-      ],
-      resolve: {
-         extensions: ['.js', '.vue'],
-      },
-   });
+mix.js('resources/js/app.js', 'public/js')   
+   .vue();
